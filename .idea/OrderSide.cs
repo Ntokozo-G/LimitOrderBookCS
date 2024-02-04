8799dc10425517db2035d56febfa43c1c5f0e2e8
Ntokozo-G <127427120+Ntokozo-G@users.noreply.github.com>
2024-02-04 21:29:06 +0200

    Update OrderSide.cs

--- .idea/OrderSide.cs
+++ .idea/OrderSide.cs
@@ -5,21 +5,4 @@ namespace Order
         BID,
         OFFER
     }
-
-    // public class OrderSideLabel
-    // {
-    //     public string Label { get; }
-    //
-    //     // public OrderSideLabel(string label)
-    //     // {
-    //     //     Label = label;
-    //     // }
-    //
-    //     // public override string ToString()
-    //     // {
-    //     //     return "OrderSideLabel{" +
-    //     //            "Label='" + Label + '\'' +
-    //     //            '}';
-    //     // }
-    // }
 }
